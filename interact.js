3135a494f1876cd11b669c571aaf83f975cd114b
danial <danial.safaei@outlook.com>
2023-12-05 13:31:11 +0330

    First

---
+++ interact.js
@@ -0,0 +1,9 @@
+// Example: A script to dynamically show/hide sections
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
